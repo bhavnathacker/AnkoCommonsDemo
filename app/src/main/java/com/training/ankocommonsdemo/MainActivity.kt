de34414c6fb6b45347bf8a3b773a1565ae22caf8
BHAVNA THACKER <bhavnathacker14@gmail.com>
2019-01-03 17:20:31 +0530

    Changes for Anko commons addtion

--- app/src/main/java/com/training/ankocommonsdemo/MainActivity.kt
+++ app/src/main/java/com/training/ankocommonsdemo/MainActivity.kt
@@ -12,6 +12,9 @@ import android.view.MenuItem
 import android.widget.Toast
 
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.*
+import org.jetbrains.anko.design.longSnackbar
+import org.jetbrains.anko.design.snackbar
 
 private const val TAG = "MainActivity"
 
@@ -26,7 +29,14 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         fab.setOnClickListener {
-            openProductActivity()
+            //openProductActivity()
+            //openLink()
+            //shareContents()
+            //shareContentsViaEmail("test@gmail.com", "Test Subject",  "Test Text" )
+            //showToast()
+            //showAlert()
+            //showSnackBar()
+            showSelector()
         }
     }
 
@@ -47,68 +57,79 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun openProductActivity() {
-        val intent = Intent(this, ProductActivity::class.java)
-        startActivity(intent)
+/*        val intent = Intent(this, ProductActivity::class.java)
+        startActivity(intent)*/
+        startActivity<ProductActivity>()
     }
 
 
     private fun openProductActivityWithOneParam() {
-        val intent = Intent(this, ProductActivity::class.java)
+/*        val intent = Intent(this, ProductActivity::class.java)
         intent.putExtra("id", 7)
-        startActivity(intent)
+        startActivity(intent)*/
+        startActivity<ProductActivity>("id" to 7)
 
     }
 
     private fun openProductActivityWithTwoParams() {
-        val intent = Intent(this, ProductActivity::class.java)
+/*        val intent = Intent(this, ProductActivity::class.java)
         intent.putExtra("id", 7)
         intent.putExtra("name", "android")
-        startActivity(intent)
+        startActivity(intent)*/
+        startActivity<ProductActivity>("id" to 7, "name" to "android")
 
     }
 
 
     private fun openProductActivityWithFlag() {
-        val intent = Intent(this, ProductActivity::class.java)
+/*        val intent = Intent(this, ProductActivity::class.java)
         intent.putExtra("id", 7)
         intent.putExtra("name", "android")
         intent.flags = Intent.FLAG_ACTIVITY_SINGLE_TOP
-        startActivity(intent)
+        startActivity(intent)*/
+        startActivity(intentFor<ProductActivity>("id" to 7, "name" to "android").singleTop())
+
     }
 
     private fun openLink() {
-        val intent = Intent(Intent.ACTION_VIEW)
+/*        val intent = Intent(Intent.ACTION_VIEW)
         intent.data = Uri.parse("http://www.google.com")
-        startActivity(intent)
+        startActivity(intent)*/
+        browse("http://www.google.com")
     }
 
 
     private fun shareContents() {
-        val shareIntent = Intent(Intent.ACTION_SEND)
+/*        val shareIntent = Intent(Intent.ACTION_SEND)
         shareIntent.type = "text/plain"
         shareIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Sample Subject")
         shareIntent.putExtra(android.content.Intent.EXTRA_TEXT, "Sample Contents")
-        startActivity(Intent.createChooser(shareIntent, null))
+        startActivity(Intent.createChooser(shareIntent, null))*/
+        share("Sample Contents", "Sample Subject")
     }
 
 
     private fun shareContentsViaEmail(address: String, subject: String, text: String) {
-        val intent = Intent(Intent.ACTION_SENDTO)
+/*        val intent = Intent(Intent.ACTION_SENDTO)
         intent.data = Uri.parse("mailto:")
         intent.putExtra(Intent.EXTRA_EMAIL, arrayOf(address))
         intent.putExtra(Intent.EXTRA_SUBJECT, subject)
         intent.putExtra(Intent.EXTRA_TEXT, text)
-        startActivity(intent)
+        startActivity(intent)*/
+        email(address, subject, text)
     }
 
 
     private fun showToast() {
-        Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show()
+        //Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show()
+        //toast("Hello")
+        //longToast("Hello")
+        toast(R.string.app_name)
     }
 
 
     private fun showAlert() {
-        val alert = AlertDialog.Builder(this)
+/*        val alert = AlertDialog.Builder(this)
         alert.setTitle("Title")
         alert.setMessage("Message")
 
@@ -118,18 +139,33 @@ class MainActivity : AppCompatActivity() {
         alert.setNegativeButton("No") { _, _ ->
             Toast.makeText(this, "No Clicked", Toast.LENGTH_SHORT).show()
         }
-        alert.create().show()
+        alert.create().show()*/
+        alert("Message"){
+
+            yesButton {
+                toast("Yes Clicked")
+            }
+
+            noButton{
+                toast("No clicked")
+            }
+        }.show()
 
 
     }
 
     private fun showSnackBar() {
-        Snackbar.make(rootView, "Welcome my Friend", Snackbar.LENGTH_SHORT).show()
+        //Snackbar.make(rootView, "Welcome my Friend", Snackbar.LENGTH_SHORT).show()
+        //rootView.snackbar("Welcome my friend")
+        //rootView.longSnackbar("Welcome")
+        rootView.snackbar("Welcome", "Click me") {
+            toast("Hi There!")
+        }
     }
 
 
     private fun showSelector() {
-        val builder = AlertDialog.Builder(this)
+/*        val builder = AlertDialog.Builder(this)
         builder.setTitle("Choose an animal")
 
         val animals = arrayOf("horse", "cow", "camel", "sheep", "goat")
@@ -143,6 +179,11 @@ class MainActivity : AppCompatActivity() {
             }
         }
         val dialog = builder.create()
-        dialog.show()
+        dialog.show()*/
+
+        val animals = listOf("horse", "cow", "camel", "sheep", "goat")
+        selector("Choose an animal", animals, {
+            dialogInterface, i -> toast("${animals[i]}")
+        })
     }
 }
